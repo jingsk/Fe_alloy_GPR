6dfdcb4dbf87309ac2ca6356a95b8015b217a693
jingsk <jingsk129@gmail.com>
2024-08-31 13:54:30 -0700

    add_feature now contains all relevant features I am inspecting. Corrected typos in NOTES.md. fit_Tc.ipynb now uses add_feature from utils.

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,5 @@
 check out 
 - https://hackingmaterials.lbl.gov/matminer/matminer.featurizers.composition.html#matminer.featurizers.composition.orbital.ValenceOrbital
-- https://hackingmaterials.lbl.gov/matminer/matminer.featurizers.composition.html#matminer.featurizers.composition.orbital.ValenceOrbital
-- https://hackingmaterials.lbl.gov/matminer/matminer.featurizers.composition.html#matminer.featurizers.composition.orbital.ValenceOrbital
-- https://hackingmaterials.lbl.gov/matminer/matminer.featurizers.composition.html#matminer.featurizers.composition.orbital.ValenceOrbital
+- https://hackingmaterials.lbl.gov/matminer/matminer.featurizers.composition.html#matminer.featurizers.composition.composite.Meredig
+- https://hackingmaterials.lbl.gov/matminer/matminer.featurizers.composition.html#matminer.featurizers.composition.alloy.WenAlloys
+- https://hackingmaterials.lbl.gov/matminer/matminer.featurizers.composition.html#matminer.featurizers.composition.alloy.YangSolidSolution
--- utils/add_feature.py
+++ utils/add_feature.py
@@ -14,3 +14,28 @@ def add_magpie(df, composition_col_name="composition"):
     magpie_properties = ElementProperty.from_preset(preset_name='magpie')
     df = magpie_properties.featurize_dataframe(df, "composition")
     return df
+
+def add_valence_orbitals(df, composition_col_name="composition"):
+    from matminer.featurizers.composition.orbital import ValenceOrbital
+    val_orb_properties = ValenceOrbital()
+    df = val_orb_properties.featurize_dataframe(df, "composition")
+    return df
+
+def add_Wen_alloys(df, composition_col_name="composition"):
+    from matminer.featurizers.composition.alloy import WenAlloys
+    wen_alloy_properties = WenAlloys()
+    df = wen_alloy_properties.featurize_dataframe(df, "composition")
+    return df
+
+def add_Yang_alloys(df, composition_col_name="composition"):
+    from matminer.featurizers.composition.alloy import YangSolidSolution
+    yang_alloy_properties = YangSolidSolution()
+    df = yang_alloy_properties.featurize_dataframe(df, "composition")
+    return df
+
+def add_Meredig(df, composition_col_name="composition"):
+    from matminer.featurizers.composition import Meredig
+    meredig_properties = Meredig()
+    df = meredig_properties.featurize_dataframe(df, "composition")
+    return df
+
