fd197d2ef1c4232c9e3ba07b92cb3d0f277becf9
jingsk <jingsk129@gmail.com>
2024-09-02 18:57:07 -0700

    class NormalizeElementFractions now normalize element fractions

--- utils/model.py
+++ utils/model.py
@@ -1,5 +1,9 @@
 import gpytorch
 import torch
+from botorch.models.transforms.input import InputTransform
+from torch import Tensor
+from torch.nn import Module 
+import numpy as np
 
 def trainGP(model, X_train, mll, optimizer, num_epochs):
     #num_epochs = 3000
@@ -32,3 +36,30 @@ def evaluateGP(model, X):
         # compute posterior
         posterior = model.posterior(X)
     return posterior.mean.squeeze().numpy(), posterior.stddev.numpy()
+
+
+class NormalizeElementFractions(InputTransform,Module):
+    def __init__(
+        self,
+        #d,
+        indices,
+        transform_on_train: bool = True,
+        transform_on_eval: bool = True,
+        transform_on_fantasize: bool = True,
+    ):
+        super().__init__()
+        self.indices = np.array(indices)
+        self.transform_on_train = transform_on_train
+        self.transform_on_eval = transform_on_eval
+        self.transform_on_fantasize = transform_on_fantasize
+    def transform(self, X: Tensor) -> Tensor:
+        r"""Transform the inputs to a model.
+
+        Args:
+            X: A `batch_shape x n x d`-dim tensor of inputs.
+
+        Returns:
+            A `batch_shape x n x d`-dim tensor of transformed inputs.
+        """
+        norm_factor = torch.sum(X[:,self.indices], axis=1)
+        return X[:,self.indices]/norm_factor[:,None]
